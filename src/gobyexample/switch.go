8e9f6b458dfe8016cf8334e907afd0f2881e87fd
Don Johnson <copyleftdev@gmail.com>
2015-11-11 06:48:32 -0800

    completed switch drill

---
+++ src/gobyexample/switch.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+
+	i := 2
+	fmt.Println("write", i, " as ")
+
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	case 4:
+		fmt.Println("four")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("it's the weekend")
+	default:
+		fmt.Println("it's a weekday :()")
+	}
+}
